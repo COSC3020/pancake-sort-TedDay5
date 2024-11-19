4d651e2136c982f4b0a7f6d7cf0a92d4f10ea33f
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-11-19 13:16:39 -0700

    Implemented flip, pancakeSort, and largest

--- code.js
+++ code.js
@@ -1,8 +1,29 @@
 function flip(array, n) {
+    for (i = 0; i < Math.floor(n / 2); i++) {
+        [array[i], array[n - (i + 1)]] = [array[n - (i + 1)], array[i]];
+    }
     return array;
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    let max = 0;
+
+    for (i = 0; i < array.length; i++) {
+        max = largest(array, array.length - i);
+        flip(array, max + 1);
+        flip(array, array.length - i);
+    }
     return array;
+}
+
+function largest(array, n) {
+    let max = 0;
+
+    for (i = 0; i < n; i++) {
+        if(array[i] > array[max]) {
+            max = i;
+        }
+    }
+    return max;
 }
